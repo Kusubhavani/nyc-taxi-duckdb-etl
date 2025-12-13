53a7b991a691a7cc35048c1f6aac11fd725e490e
Kusu Bhavani <bhavanikusu92@gmail.com>
2025-12-13 20:56:07 +0530

    Add SQL queries for average fare, revenue, and trips

--- queries/queries.sql
+++ queries/queries.sql
@@ -1,1 +1,15 @@
+-- Query 1: Average fare by passenger count
+SELECT passenger_count, AVG(fare_amount)
+FROM fact_trips
+GROUP BY passenger_count;
+
+-- Query 2: Total revenue per pickup location
+SELECT pickup_location_id, SUM(total_amount)
+FROM fact_trips
+GROUP BY pickup_location_id;
+
+-- Query 3: Trips per day
+SELECT date(tpep_pickup_datetime) AS trip_date, COUNT(*)
+FROM fact_trips
+GROUP BY trip_date;
 
